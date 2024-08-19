8b570b95aa13ca00211d5a1dc9d9f548dae76e71
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-08-20 08:05:08 +0900

    add files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,28 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // NOTE: siteがないとエラー吐くので削除しない
+  site: "https://www.pentio.com/",
+  integrations: [
+    // NOTE: 自動でサイトマップを作成してくれるプラグイン
+    sitemap(),
+  ],
+  server: {
+    // NOTE: npm run devなどで開発環境を立ち上げたときのポート番号 デフォルト値は4321
+    port: 3000,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/components",
+        "@scss": "/src/scss",
+        "@layouts": "/src/layouts",
+      },
+    },
+  },
+  build: {
+    format: "preserve", // NOTE: 出力ファイルの設定
+  },
+});
