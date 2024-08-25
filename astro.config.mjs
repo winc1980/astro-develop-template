3c7dcbdf6ef9301c55cb8e7bda470543ef1d5b1e
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-08-25 17:13:21 +0900

    edit astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // NOTE: siteにはデプロイするGitHub Organizationのドメインを書く(例：https://winc1980.github.io)
-  site: "",
-  // NOTE: baseにはスラッシュとレポジトリ名を書く(例：/official)
-  base: "",
-  integrations: [
-    // NOTE: 自動でサイトマップを作成してくれるプラグイン
-    sitemap(),
-  ],
   vite: {
     resolve: {
       alias: {
