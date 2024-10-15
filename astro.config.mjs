9af4de1a83a234dfb197131044b97ccafe5512e6
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-10-15 21:48:29 +0900

    install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        "@": "/src",
-      },
-    },
+        "@": "/src"
+      }
+    }
   },
   build: {
-    format: "preserve", // NOTE: 出力ファイルの設定
+    format: "preserve" // NOTE: 出力ファイルの設定
   },
+  integrations: [tailwind()]
 });
